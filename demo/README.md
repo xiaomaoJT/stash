7f20d2e5b63e28028e1d576c91a86fdfb45845f8
XiaoMao <>
2023-05-18 11:17:13 +0800

    test

---
+++ demo/README.md
@@ -0,0 +1,13 @@
+## Stash配置样例
+
+------
+
+
+
+##### 🏆 Default_Stash.yaml
+
+> Stash 初始化 默认配置
+
+##### 🏆 Default_WiKi.yaml
+
+> Stash 用户手册 配置样例
---
+++ script/XiaoMaoPic.js
@@ -0,0 +1,27 @@
+$httpClient.get(
+  "https://api.lolicon.app/setu?apikey=&r18=2&keyword=&num=1",
+  (error, response, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+      let obj = JSON.parse(data);
+      if (obj.code == 0 && obj.data.length) {
+        let picture = encodeURI(obj.data[0].url);
+
+        $notification.post(
+          "💢靓图推送·" + obj.data[0].title || "💢每日靓图",
+          obj.data[0].author ||
+            "" + obj.data[0].tags[0] ||
+            "" + obj.data[0].tags[1] ||
+            "",
+          {url:picture}
+        );
+      }
+    }
+  }
+);
+
+setTimeout(() => {
+  $done({});
+}, 10000);
